63838ca1ffda7c20aef42a5a7e992a1767d7c787
jsarath <jsarath@uncc.edu>
2021-09-28 14:43:49 -0400

    Update webpage.js

--- itis3135/scripts/webpage.js
+++ itis3135/scripts/webpage.js
@@ -1,11 +1,11 @@
 ///Variables to get users to enter name and feelings
-let name = prompt("What is your name?");
-let feeling = prompt("How are you feeling?");
+var name = prompt("What is your name?");
+var feeling = prompt("How are you feeling?");
 
 //Get date information
-let today = new Date();
-let date = (today.getMonth()+1) +"-"+ today.getDate()+"-"+today.getFullYear();
-let time = today.getHours()+":" today.getMinutes();
+var today = new Date();
+var date = (today.getMonth()+1) +"-"+ today.getDate()+"-"+today.getFullYear();
+var time = today.getHours()+":" today.getMinutes();
 
 //access specific elements quickly
 document.getElementById("name").innerHTML = name;
