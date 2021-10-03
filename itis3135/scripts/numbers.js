1e52fe910832315c96395cd20205c4ce9724ae8f
jsarath <jsarath@uncc.edu>
2021-10-03 18:24:22 -0400

    assignment 7
    Revert changes

--- itis3135/scripts/numbers.js
+++ itis3135/scripts/numbers.js
@@ -1,47 +1,45 @@
-/* */
-
 let sides = prompt("Jawless Snakes needs a number between 1-10.");
 //Get and set a basic shape
-document.getElementById("shapes").innerHTML = sides + " sided polygon"; 
+document.getElementById("shape").innerHTML = sides + " sided polygon"; 
 
-function shapes() {
+function shape() {
     //Get integer input for side
     sides = parseInt(prompt("How many sides do you want for your shape?")); s
-    //check num of side and change text to match the number
+    //check num of side and display shape
     switch (sides) { 
         case 1:
-            document.getElementById("shapes").innerHTML = "monogon";
+            document.getElementById("shape").innerHTML = "monogon";
             break;
         case 2:
-            document.getElementById("shapes").innerHTML = "digon";
+            document.getElementById("shape").innerHTML = "digon";
             break;
         case 3:
-            document.getElementById("shapes").innerHTML = "trigon";
+            document.getElementById("shape").innerHTML = "trigon";
             break;
         case 4:
-            document.getElementById("shapes").innerHTML = "tetragon";
+            document.getElementById("shape").innerHTML = "tetragon";
             break;
         case 5:
-            document.getElementById("shapes").innerHTML = "pentagon";
+            document.getElementById("shape").innerHTML = "pentagon";
             break;
         case 6:
-            document.getElementById("shapes").innerHTML = "hexagon";
+            document.getElementById("shape").innerHTML = "hexagon";
             break;
         case 7:
-            document.getElementById("shapes").innerHTML = "heptagon";
+            document.getElementById("shape").innerHTML = "heptagon";
             break;
         case 8:
-            document.getElementById("shapes").innerHTML = "octagon";
+            document.getElementById("shape").innerHTML = "octagon";
             break;
         case 9:
-            document.getElementById("shapes").innerHTML = "enneagon";
+            document.getElementById("shape").innerHTML = "enneagon";
             break;
         case 10:
-            document.getElementById("shapes").innerHTML = "decagon";
+            document.getElementById("shape").innerHTML = "decagon";
             break;
 
-        default: //tell user if input is incorrect and try again
+        default: //tell user if input is incorrect and ask again
             alert("Way to break the machine! Try Again! Need a number between 1 and 10.");
-            shapes()
+            shape();
     }
 }
