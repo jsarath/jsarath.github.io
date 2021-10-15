09d984059a3664ed3e9fec431cfbba63d4ea722a
jsarath <jsarath@uncc.edu>
2021-10-15 12:41:10 -0400

    Update Practice.js

--- itis3135/scripts/Practice.js
+++ itis3135/scripts/Practice.js
@@ -3,13 +3,30 @@ var $ = function(id) {
 };
 
 window.onload = function() {
-
-
+	//item capp
 	//add onclick event handler for each image
-
+	document.getElementById("cappuccino").onclick=function(){
+		document.getElementById("order").append('$3.45 - Cappuccino - Delicious Cappuccino.');};
 	// for click event add item to order and update total
-
+	document.getElementById("cappuccino").onclick=function(){
+		total = (parseFloat(total) + parseFloat(3.45)).toFixed(2);};
 	// display order and total
+	document.getElementById("cappuccino").onclick=function(){
+		document.getElementById("total").innerHTML="<em>Total:"+total+" </em>";
+	};
+
+
+	//item espr
+	//add onclick event handler for each image
+	document.getElementById("espresso").onclick=function(){
+		document.getElementById("order").append('$1.95 - Espresso - Delicious espresso. Wanna try it?');};
+	// for click event add item to order and update total
+	document.getElementById("espresso").onclick=function(){
+		total = (parseFloat(total) + parseFloat(1.95)).toFixed(2);};
+	// display order and total
+	document.getElementById("espresso").onclick=function(){
+		document.getElementById("total").innerHTML="<em>Total:"+total+" </em>";
+	};
 
 	
 		
