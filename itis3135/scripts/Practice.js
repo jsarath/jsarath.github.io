2bde086fd9e969c72073ee9952860206cbdf993f
jsarath <jsarath@uncc.edu>
2021-10-15 12:51:28 -0400

    Update Practice.js

--- itis3135/scripts/Practice.js
+++ itis3135/scripts/Practice.js
@@ -19,10 +19,8 @@ window.onload = function() {
 	document.getElementById("espresso").onclick=function(){
 		document.getElementById("order").append('$1.95 - Espresso - Delicious espresso. Wanna try it?');};
 	// for click event add item to order and update total
-	document.getElementById("espresso").onclick=function(){
-		total = (parseFloat(total) + parseFloat(1.95)).toFixed(2);};
+		total = (parseFloat(total) + parseFloat(1.95)).toFixed(2);
 	// display order and total
-	document.getElementById("espresso").onclick=function(){
 		document.getElementById("total").innerHTML="<em>Total:"+total+" </em>";
 	};
 
