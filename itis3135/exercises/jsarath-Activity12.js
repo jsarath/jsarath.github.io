ae182a8984a55f0c7f59435abdff394475b9ca4c
jsarath <71343908+jsarath@users.noreply.github.com>
2021-11-15 15:32:35 -0500

    Update jsarath-Activity12.js

--- itis3135/exercises/jsarath-Activity12.js
+++ itis3135/exercises/jsarath-Activity12.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     $("#slider").bxSlider({
+        randomStart: true,
         auto: true,
         minSlides: 1,
         maxSlides: 1,
-        randomStart: true,
         slideWidth: 500,
         slideMargin: 20,
-        shrinkItems: true,
+        captions: true,
         pause: 3000,
-        captions: true
+        pager: true,
+        pagerType: "short"
     });
 });
